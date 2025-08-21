dde71101fb74d666818ea4438f3d3e1e1182f0cf
Armid Alizadeh <107582502+ARMID2003@users.noreply.github.com>
2025-08-21 17:19:15 +0200

    Update test.py

--- test.py
+++ test.py
@@ -36,12 +36,48 @@
 # weight = input("weight: ")
 # unit = input ('(k)Kg or (L)bs: ') 
 
-Tempeger = int(input("Enter Tempeger: "))
-unit = input("(F)Farendheit or (C)Cantigrand: ")
-if unit.upper == "F":
-    print(float(Tempeger -32) / 1.8 )
-if unit.upper == "F":
-    print(float(Tempeger * 1.8 + 32) )
+# Tempeger = int(input("Enter Tempeger: "))
+# unit = input("(F)Farendheit or (C)Cantigrand: ")
+# if unit.upper == "F":
+#     print(float(Tempeger -32) / 1.8 )
+
+# if unit.upper == "C":
+#     print(float(Tempeger * 1.8 + 32) )
 
 
+# print(""" That mean if you type with                      space thast show , 
+#       that waht 
+#       it is
+#       """)
 
+
+# yelling = anstreiten not worked (duplicated)
+#schleifen  == Loop  --> for While
+
+# commend = ""
+# start = False
+# stop = False
+
+# while commend != "QUIT":
+#     commend = input("> ").upper()
+#     if commend == "HELP":
+#         print("Start - Start the car\n"
+#               "Stop - Stop the car\n"
+#               "Quit - Leave the games")
+        
+#     elif commend == "START":
+#         if start:
+#             print("The car is already on")
+#         else:
+#             start == True
+#             print("The car is on")
+    
+
+#     elif commend == "STOP":
+#         print("The car is stopped")
+#     elif commend == "QUIT":
+#         print("See You Soon")
+#     else :
+#         print("I don't undrestand , what you mean !" )
+
+  
