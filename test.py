18dd1b1a03697e690d94109aab3d62f25b2feb28
Armid Alizadeh <107582502+ARMID2003@users.noreply.github.com>
2025-08-21 16:20:13 +0200

    Add files via upload

---
+++ test.py
@@ -0,0 +1,47 @@
+# * bazieee
+
+# i  = 1 
+
+# while i <= 10:  
+#     Star = i * "*"  
+#     print( i * '*' , i )
+#     i +=1 
+#    ----------------------------
+
+
+# bazie guss number ba code tamiz
+
+# number = 9
+# attemt = 0
+
+# while attemt < 3:
+#     guss_number = int(input("Guss number: "))
+#     attemt += 1
+#     if guss_number == number:
+#         print("You win")
+#         break
+# else :
+#     print("You lose")
+
+
+
+
+
+
+# while attemt == 3:
+#     print("You lose")
+#     attemt +=1
+
+
+# weight = input("weight: ")
+# unit = input ('(k)Kg or (L)bs: ') 
+
+Tempeger = int(input("Enter Tempeger: "))
+unit = input("(F)Farendheit or (C)Cantigrand: ")
+if unit.upper == "F":
+    print(float(Tempeger -32) / 1.8 )
+if unit.upper == "F":
+    print(float(Tempeger * 1.8 + 32) )
+
+
+
